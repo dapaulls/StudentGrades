f17ef0f50273a465b7a7f6f585dc075c6912a522
dapaulls <dapaulls@gmail.com>
2014-11-24 14:05:34 +0000

    Student Grades Calculator
    Calculates overall course mark based on subject marks input

---
+++ StudentGrades/Program.cs
@@ -0,0 +1,35 @@
+﻿/* HND Computing and Systems Development
+ * Unit 19 - Object Orientated Programming
+ * Semester 2, June - October 2013 (updated August 2014)
+ * by David Paulls
+ * This program calculates a student's grades and final score from the subject marks input.
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StudentGrades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            do
+            {
+                Subjects newStudent = new Subjects();
+
+                Console.Clear();
+                Console.WriteLine("STUDENT'S GRADE CALCULATION PROGRAM");
+                Console.WriteLine("-----------------------------------\n");
+
+                newStudent.InputNoOfSubjects();
+                newStudent.EnterSubjectAndMark();
+                newStudent.CalcFinalAverage(newStudent.CourseTotal, newStudent.NoOfSubjects);
+
+                Console.WriteLine("\nWould you like to run the program again? ('Y' for yes, any other key to quit.)");
+            }
+            while (Console.ReadLine().ToLower() == "y");
+        }
+    }
+}
---
+++ StudentGrades/ValidateInput.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StudentGrades
+{
+    // A static class to check the user's input is a valid integer
+    static class ValidateInput
+    {
+        static private bool result;
+        static private int inputNum;
+
+        static public int InputNum
+        {
+            get { return inputNum; }
+            set { inputNum = value; }
+        }
+
+        // Method to check that the value input is an integer and between the allowed values
+        static public int CheckInt(string promptText, int min, int max)
+        {
+        tryAgain:
+            Console.WriteLine("Please enter a {0} between {1} and {2}: ", promptText, min, max);
+            result = int.TryParse(Console.ReadLine(), out inputNum);
+            if (!result || (inputNum < min || inputNum > max))
+            {
+                goto tryAgain;
+            }
+            return inputNum;
+        }
+
+    }
+}
